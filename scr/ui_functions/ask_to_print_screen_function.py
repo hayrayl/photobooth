9f0ca3ef0a1b85fd9b99d72903bbb769430c5eb3
Haylie Rayl <hcrayl@purdue.edu>
2026-02-15 15:56:40 -0500

    printing testing

--- scr/ui_functions/ask_to_print_screen_function.py
+++ scr/ui_functions/ask_to_print_screen_function.py
@@ -11,6 +11,8 @@ class AskToPrintScreen(QtWidgets.QWidget, Ui_Ask_Print):
         super().__init__(parent)
         self.setupUi(self)
         self.main_window = main_window
+        
+        self.strip_path = None  # ADD THIS
 
         self.design_setup()
         self.connect_signals()
@@ -24,13 +26,58 @@ class AskToPrintScreen(QtWidgets.QWidget, Ui_Ask_Print):
         self.pushButton_yes.clicked.connect(self.go_to_print)
         self.pushButton_no.clicked.connect(self.go_to_home)
 
+    def set_strip(self, strip_path):
+        """Set the photo strip to print"""
+        self.strip_path = strip_path
+        print(f"Strip path set: {strip_path}")
+
+    def showEvent(self, event):
+        """Check printer status when screen appears"""
+        super().showEvent(event)
+        
+        # Re-apply color scheme
+        utils_screen.set_background(self.background, self.main_window.color_scheme)
+        utils_screen.style_all_buttons(self, self.main_window.color_scheme)
+        utils_screen.style_all_labels(self, self.main_window.color_scheme)
+        
+        # If no printer connected, skip straight to home
+        if not self.main_window.printer_manager.is_printer_connected():
+            print("No printer connected - skipping print screen")
+            QtCore.QTimer.singleShot(0, self.go_to_home)
+            return
+        
+        # Printer is connected - show screen normally
+        self.pushButton_yes.setEnabled(True)
+        self.pushButton_yes.setText("Yes, print it!")
+        self.label_main.setText("Would you like to print\nyour photo strip?")
+
     def go_to_print(self):
-        """User wants to print - handle printing"""
-        print("User wants to print photos")
-        # TODO: Implement printing functionality
-        self.go_to_home()
+        """User wants to print"""
+        if not self.strip_path:
+            print("ERROR: No strip path set")
+            self.go_to_home()
+            return
+        
+        # Show printing message
+        self.label_main.setText("Printing...")
+        self.pushButton_yes.setEnabled(False)
+        self.pushButton_no.setEnabled(False)
+        QtWidgets.QApplication.processEvents()
+        
+        # Print the strip
+        success = self.main_window.printer_manager.print_photo_strip(self.strip_path)
+        
+        if success:
+            self.label_main.setText("Printing! ✓")
+            QtCore.QTimer.singleShot(2000, self.go_to_home)
+        else:
+            self.label_main.setText("Print failed.\nTry again?")
+            self.pushButton_yes.setEnabled(True)
+            self.pushButton_no.setEnabled(True)
 
     def go_to_home(self):
         """Go back to home screen"""
-        print("User declined print")
+        self.pushButton_yes.setEnabled(True)
+        self.pushButton_no.setEnabled(True)
+        self.pushButton_yes.setText("Yes, print it!")
         self.parentWidget().setCurrentIndex(1)
